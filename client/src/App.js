9c19ddb09d6fdd17a25086950296e6a9ef0f2547
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-08-15 10:56:53 +0200

    add contacts, conversations

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,16 @@ import Login from './components/Login'
 import Join from './components/Join'
 import useLocalStorage from './hooks/useLocalStorage'
 import Dashboard from './components/Dashboard'
+import { ContactsProvider } from './contexts/ContactsContext'
 
 function App() {
   const [id, setId] = useLocalStorage('id')
-  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />
+  const dashboard = (
+    <ContactsProvider>
+      <Dashboard id={id} />
+    </ContactsProvider>
+  )
+  return id ? dashboard : <Login onIdSubmit={setId} />
 }
 
 export default App
--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
+
 const PREFIX = 'whatsapp-clone-'
 
 export default function useLocalStorage(key, initialValue) {
@@ -7,13 +8,15 @@ export default function useLocalStorage(key, initialValue) {
     const jsonValue = localStorage.getItem(prefixedKey)
     if (jsonValue != null) return JSON.parse(jsonValue)
     if (typeof initialValue === 'function') {
-      return initialValue
+      return initialValue()
     } else {
       return initialValue
     }
   })
+
   useEffect(() => {
     localStorage.setItem(prefixedKey, JSON.stringify(value))
   }, [prefixedKey, value])
+
   return [value, setValue]
 }
