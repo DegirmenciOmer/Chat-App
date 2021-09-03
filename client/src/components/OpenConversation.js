c0eb69019b85154be28573c6a5c68ed6cc3a008e
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-09-04 00:02:17 +0200

    display conversations, style login page

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -1,21 +1,62 @@
-import React, { useState } from 'react'
-import { Button, Form, InputGroup } from 'react-bootstrap'
+import React, { useState, useCallback } from 'react'
+import { Form, InputGroup, Button } from 'react-bootstrap'
 import { useConversations } from '../contexts/ConversationsProvider'
 
-const OpenConversation = () => {
+export default function OpenConversation() {
   const [text, setText] = useState('')
+  const setRef = useCallback((node) => {
+    if (node) {
+      node.scrollIntoView({ smooth: true })
+    }
+  }, [])
   const { sendMessage, selectedConversation } = useConversations()
-  const handleSubmit = (e) => {
+
+  function handleSubmit(e) {
     e.preventDefault()
+
     sendMessage(
       selectedConversation.recipients.map((r) => r.id),
       text
     )
     setText('')
   }
+
   return (
     <div className='d-flex flex-column flex-grow-1'>
-      <div className='flex-grow-1 overflow-auto'></div>
+      <div className='flex-grow-1 overflow-auto'>
+        <div className='d-flex flex-column align-items-end justify-content-end px-3'>
+          {selectedConversation.messages.map((message, index) => {
+            const lastMessage =
+              selectedConversation.messages.length - 1 === index
+            return (
+              <div
+                ref={lastMessage ? setRef : null}
+                key={index}
+                className={`my-1 d-flex flex-column ${
+                  message.fromMe
+                    ? 'align-self-end align-items-end'
+                    : 'align-items-start'
+                }`}
+              >
+                <div
+                  className={`rounded px-2 py-1 ${
+                    message.fromMe ? 'bg-primary text-white' : 'border'
+                  }`}
+                >
+                  {message.text}
+                </div>
+                <div
+                  className={`text-muted small ${
+                    message.fromMe ? 'text-right' : ''
+                  }`}
+                >
+                  {message.fromMe ? 'You' : message.senderName}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
       <Form onSubmit={handleSubmit}>
         <Form.Group className='m-2'>
           <InputGroup>
@@ -27,9 +68,7 @@ const OpenConversation = () => {
               style={{ height: '75px', resize: 'none' }}
             />
             <InputGroup.Append>
-              <Button style={{ height: '75px', resize: 'none' }} type='submit'>
-                Send
-              </Button>
+              <Button type='submit'>Send</Button>
             </InputGroup.Append>
           </InputGroup>
         </Form.Group>
@@ -37,5 +76,3 @@ const OpenConversation = () => {
     </div>
   )
 }
-
-export default OpenConversation
