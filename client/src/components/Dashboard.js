beff72b4e7a3a846916bb28d337a7cfdf7a34b63
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-09-14 22:45:11 +0200

    if no contacts yet, encourage the user to get one; add sidebar context and implement it in related components

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,14 +2,22 @@ import React from 'react'
 import Sidebar from './Sidebar'
 import OpenConversation from './OpenConversation'
 import { useConversations } from '../contexts/ConversationsProvider'
+import { Button } from 'react-bootstrap'
+import { useToggleSidebar } from './../contexts/ToggleSidebarProvider'
 
 export default function Dashboard({ id }) {
   const { selectedConversation } = useConversations()
+  const { toggleSidebar } = useToggleSidebar()
 
   return (
-    <div className='d-flex' style={{ height: '100vh' }}>
-      <Sidebar id={id} />
-      {selectedConversation && <OpenConversation />}
-    </div>
+    <>
+      <Button className='menu-btn' variant='primary' onClick={toggleSidebar}>
+        Menu
+      </Button>
+      <div className='d-flex' style={{ height: '100vh' }}>
+        <Sidebar id={id} />
+        {selectedConversation && <OpenConversation />}
+      </div>
+    </>
   )
 }
