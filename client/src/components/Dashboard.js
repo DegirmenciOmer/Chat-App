c8970b9693376b6bb0c46f2b9e5ef55b382b0b8f
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-09-21 09:40:42 +0200

    fix fullscreen bg in login, add IntroModal

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,22 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { Modal, Button } from 'react-bootstrap'
 import Sidebar from './Sidebar'
 import OpenConversation from './OpenConversation'
 import Header from './Header'
 
 import { useConversations } from '../contexts/ConversationsProvider'
-import { useToggleSidebar } from '../contexts/ToggleSidebarProvider'
+import IntroModal from './IntroModal'
 
 export default function Dashboard({ id }) {
   const [show, setShow] = useState(false)
 
-  const { toggleSidebar } = useToggleSidebar()
   const { selectedConversation } = useConversations()
-
-  const handleClose = () => {
-    setShow(false)
-    toggleSidebar()
-  }
 
   useEffect(() => {
     !selectedConversation && setShow(true)
@@ -29,30 +22,7 @@ export default function Dashboard({ id }) {
       <div className='gradient-bg d-flex' style={{ height: '90vh' }}>
         <Sidebar id={id} />
         {selectedConversation && <OpenConversation />}
-        <Modal
-          show={show}
-          onHide={handleClose}
-          backdrop='static'
-          keyboard={false}
-        >
-          <Modal.Header>
-            <Modal.Title>Let's start!</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <ol>
-              <li>First, you need to add your friends to your contacts.</li>
-              <li>
-                Then you need to start a conversation by selecting from your
-                contacts.
-              </li>
-            </ol>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant='primary' onClick={handleClose}>
-              Understood
-            </Button>
-          </Modal.Footer>
-        </Modal>
+        <IntroModal show={show} setShow={setShow} />
       </div>
     </>
   )
